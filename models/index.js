d72fc66a56e768c71d3a0e2a30f7aaa774b58245
va2650lt <60371087+va2650lt@users.noreply.github.com>
2020-06-22 01:39:42 -0500

    log

--- models/index.js
+++ models/index.js
@@ -6,16 +6,10 @@ let config = require(__dirname + '/../config.json')[env];
 let db = {};
 
 let sequelize;
-console.log(config.use_env_variable)
-console.log(__dirname + '/../config.json')
+
 
 if (config.use_env_variable) {
-  console.log(process.env[config.use_env_variable])
-  console.log(config.use_env_variable)
-  console.log(process.env.DATABASE_URL)
-  console.log(config)
-  //sequelize = new Sequelize(process.env[config.use_env_variable], config)
-  sequelize = new Sequelize(process.env.DATABASE_URL, config)
+  sequelize = new Sequelize(process.env[config.use_env_variable], config)
 } else {
   sequelize = new Sequelize(config)
 }
